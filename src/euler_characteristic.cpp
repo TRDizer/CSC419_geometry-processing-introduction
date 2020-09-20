ea78bae6f6de7304b6987d262392e44057e37f2a
TR <konataxkagami9@gmail.com>
2020-09-20 01:28:50 -0400

    fixup eular characteristic calculation

--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -5,7 +5,7 @@ int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi = 0;
   // ADD YOUR CODE HERE
-  // Assumption: no unreferenced vertex
-  Chi = F.maxCoeff() - edges(F).rows() + F.rows();
+  // Assumption: no unreferenced vertex and F_index_v starts from 0
+  Chi = (F.maxCoeff()) + 1 - edges(F).rows() + F.rows();
   return Chi;
 }
